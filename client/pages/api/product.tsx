0e79bc7e1ad339fd3c35255928c506c2f6dd7701
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-09-15 00:46:20 +0530

    added next js project

---
+++ client/pages/api/product.tsx
@@ -0,0 +1,31 @@
+// import dbConnect from "../../../utils/mongo";
+import dbConnect from "../../utils/mongoose";
+import Product from "../../model/product";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  dbConnect();
+
+  if (method === "GET") {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+}
