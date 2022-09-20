6a0db9dd13b569d51e7c08e6a6bdc48f560a80db
milan Katira <84834795+milankatira@users.noreply.github.com>
2022-09-20 09:11:38 +0530

    added chnages

--- constant/app_config.ts
+++ constant/app_config.ts
@@ -3,7 +3,7 @@ const masterConfig = {
     server_url: "http://localhost:3000/api",
   },
   staging: {
-    server_url: "https://shop-two-sable.vercel.app/api",
+    server_url: "https://classy-blini-247a61.netlify.app/api",
   },
   prod: {
     server_url: "https://hire.inaraconsultancy.com",
